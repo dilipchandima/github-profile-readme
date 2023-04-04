dbf97ee7a668dff496926548475a28e64fa7a801
Dileepa Chandima <dilipchandima@gmail.com>
2023-04-04 22:29:05 +0530

    Add sample readme file and md content renderer

---
+++ src/app/ReadMe.md
@@ -0,0 +1,17 @@
+# 💻 Tech Stack:
+
+![Code-Igniter](https://img.shields.io/badge/CodeIgniter-%23EF4223.svg?style=for-the-badge&logo=codeIgniter&logoColor=white) ![Angular.js](https://img.shields.io/badge/angular.js-%23E23237.svg?style=for-the-badge&logo=angularjs&logoColor=white) ![Angular](https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white) ![Anaconda](https://img.shields.io/badge/Anaconda-%2344A833.svg?style=for-the-badge&logo=anaconda&logoColor=white) ![AdonisJS](https://img.shields.io/badge/adonisjs-%23220052.svg?style=for-the-badge&logo=adonisjs&logoColor=white) ![.Net](https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white) ![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white) ![Buefy](https://img.shields.io/badge/Buefy-7957D5?style=for-the-badge&logo=buefy&logoColor=48289E) ![GraphQL](https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&logo=graphql&logoColor=white) ![Dart](https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white)
+
+# 📊 GitHub Stats:
+
+![](https://github-readme-stats.vercel.app/api?username=dilipchandima&theme=blue-green&hide_border=false&include_all_commits=true&count_private=true)<br/>
+![](https://github-readme-streak-stats.herokuapp.com/?user=dilipchandima&theme=blue-green&hide_border=false)<br/>
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=dilipchandima&theme=blue-green&hide_border=false&include_all_commits=true&count_private=true&layout=compact)
+
+### ✍️ Random Dev Quote
+
+![](https://quotes-github-readme.vercel.app/api?type=horizontal&theme=radical)
+
+---
+
+[![](https://visitcount.itsvg.in/api?id=dilipchandima&icon=0&color=0)](https://visitcount.itsvg.in)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,83 @@
+import ReactMarkdown from "react-markdown";
+
+import remarkGfm from "remark-gfm";
+const markdown = `# 💻 Tech Stack:
+
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![Kotlin](https://img.shields.io/badge/kotlin-%230095D5.svg?style=for-the-badge&logo=kotlin&logoColor=white) ![LaTeX](https://img.shields.io/badge/latex-%23008080.svg?style=for-the-badge&logo=latex&logoColor=white) ![React Router](https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white) ![Apache Ant](https://img.shields.io/badge/Apache%20Ant-A81C7D?style=for-the-badge&logo=Apache%20Ant&logoColor=white) ![Plotly](https://img.shields.io/badge/Plotly-%233F4F75.svg?style=for-the-badge&logo=plotly&logoColor=white) ![Gimp Gnu Image Manipulation Program](https://img.shields.io/badge/Gimp-657D8B?style=for-the-badge&logo=gimp&logoColor=FFFFFF)
+# 📊 GitHub Stats:
+![](https://github-readme-stats.vercel.app/api?username=dilipchandima&theme=dark&hide_border=false&include_all_commits=false&count_private=false)<br/>
+![](https://github-readme-streak-stats.herokuapp.com/?user=dilipchandima&theme=dark&hide_border=false)<br/>
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=dilipchandima&theme=dark&hide_border=false&include_all_commits=false&count_private=false&layout=compact)
+
+### ✍️ Random Dev Quote
+![](https://quotes-github-readme.vercel.app/api?type=horizontal&theme=radical)
+
+---
+[![](https://visitcount.itsvg.in/api?id=dilipchandima&icon=0&color=0)](https://visitcount.itsvg.in)
+
+ Proudly created with GPRM ( https://gprm.itsvg.in )
+ 
+ ### 👋 &nbsp; Hi there I'm Dileepa Chandima (dilipchandima)
+
+- 🔭  &nbsp; I’m currently working on NextJS and ReactNative Development
+- 🌱  &nbsp; I’m currently learning NestJS, Node and Docker
+- 👯  &nbsp; I’m looking to collaborate with great ideas
+- 💬  &nbsp; Ask me about React Native and Firebase
+- 📫  &nbsp; How to reach me: dilipchandima@gmail.com
+---
+### 🌐 &nbsp; You can find me around WWW 🌐
+
+- web - https://www.dilipchandima.com/
+- linkedIn - https://www.linkedin.com/in/dilipchandima/
+- facebook - https://www.facebook.com/dilip.chandima/
+---
+### npm modules
+
+- rn-select-date-range https://www.npmjs.com/package/rn-select-date-range
+
+---
+### my projects
+- https://www.scrumtable.xyz/
+
+---
+### 🛠 &nbsp;Tech Stack
+
+![JavaScript](https://img.shields.io/badge/-JavaScript-05122A?style=flat&logo=javascript)
+![Java](https://img.shields.io/badge/-Java-05122A?style=flat&logo=Java&logoColor=FFA518)\
+![React](https://img.shields.io/badge/-React-05122A?style=flat&logo=react)
+![ReactNative](https://img.shields.io/badge/-React-05122A?style=flat&logo=react)
+![Node.js](https://img.shields.io/badge/-Node.js-05122A?style=flat&logo=node.js)
+![Bootstrap](https://img.shields.io/badge/-Bootstrap-05122A?style=flat&logo=bootstrap&logoColor=563D7C)\
+![HTML](https://img.shields.io/badge/-HTML-05122A?style=flat&logo=HTML5)
+![CSS](https://img.shields.io/badge/-CSS-05122A?style=flat&logo=CSS3&logoColor=1572B6)
+![Git](https://img.shields.io/badge/-Git-05122A?style=flat&logo=git)
+![GitHub](https://img.shields.io/badge/-GitHub-05122A?style=flat&logo=github)
+![Markdown](https://img.shields.io/badge/-Markdown-05122A?style=flat&logo=markdown)\
+![Visual Studio Code](https://img.shields.io/badge/-Visual%20Studio%20Code-05122A?style=flat&logo=visual-studio-code&logoColor=007ACC)
+![IntelliJ IDEA](https://img.shields.io/badge/-RStudio-05122A?style=flat&logo=intellijidea)\
+![Illustrator](https://img.shields.io/badge/-Illustrator-05122A?style=flat&logo=adobe-illustrator)
+![Photoshop](https://img.shields.io/badge/-Photoshop-05122A?style=flat&logo=adobe-photoshop)
+![InDesign](https://img.shields.io/badge/-InDesign-05122A?style=flat&logo=adobe-indesign)
+
+----
+### ⚙️ GitHub Analytics
+
+![](https://github-readme-stats-eight-theta.vercel.app/api?username=dilipchandima&show_icons=true&theme=calm&include_all_commits=true&count_private=true)
+
+
+<p>
+<a href="https://github.com/dilipchandima">
+  <img height="180em" src="https://github-readme-stats-eight-theta.vercel.app/api?username=dilipchandima&show_icons=true&theme=calm&include_all_commits=true&count_private=true"/>
+  <img height="180em" src="https://github-readme-stats-eight-theta.vercel.app/api/top-langs/?username=dilipchandima&layout=compact&langs_count=8&theme=calm"/>
+</a>
+</p>
+`;
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-9xl font-bold">github profile readme</h1>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
     </main>
   );
 }
